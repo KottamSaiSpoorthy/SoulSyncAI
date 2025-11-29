891c16ebcd6a30cf8c0d700fe19faa8d68bebfd7
KottamSaiSpoorthy <kottamsaispoorthy@gmail.com>
2025-11-30 03:32:39 +0530

    EMpty repo 2

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-import app from "./app.js";
-import { connectDB } from "./config/database.js";
-import { ENV } from "./config/env.js";
-
-connectDB();
-
-app.listen(ENV.PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${ENV.PORT}`);
-});
