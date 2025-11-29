7bd6a25e09bdc50a197e40616247a157b9560025
KottamSaiSpoorthy <kottamsaispoorthy@gmail.com>
2025-11-29 22:39:13 +0530

    Empty repo

--- backend/Chatdb.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-const chatschema = new mongoose.Schema({
-    messege:String,
-    reply:String,
-});
-export default mongoose.model("chat",chatschema);
--- backend/Verifyadmin.js
+++
@@ -1,5 +0,0 @@
-function verifyadmin(req,res,next){
-    if(req.user.role !== "admin") return res.json({messege:"access denied"});
-    next()
-}
-export default verifyadmin;
